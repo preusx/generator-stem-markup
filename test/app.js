0852e70e776e8e68b42e7d9ff1a4be321fbb7d7a
Alex PreusX <preusx.dev@gmail.com>
2016-01-14 06:19:57 +0200

    Unused gulp tasks removed.

--- test/app.js
+++ test/app.js
@@ -1,19 +1,30 @@
 'use strict';
-var path = require('path');
-var assert = require('yeoman-assert');
-var helpers = require('yeoman-generator').test;
+let path = require('path');
+let assert = require('yeoman-assert');
+let helpers = require('yeoman-test');
 
-describe('generator-stem-markup:app', function () {
-  before(function (done) {
+describe('generator-stem-markup:app', function() {
+  let generator;
+
+  before((done) => {
     helpers.run(path.join(__dirname, '../generators/app'))
-      .withOptions({someOption: true})
-      .withPrompts({someAnswer: true})
+      .inTmpDir()
+      .withOptions({
+        'skip-welcome-message': true,
+        'skip-install': true
+      })
+      .withPrompts({
+        githubUser: '',
+        applicationName: 't',
+      })
+      .on('ready', (instance) => {
+        generator = instance;
+      })
       .on('end', done);
   });
 
-  it('creates files', function () {
-    assert.file([
-      'dummyfile.txt'
-    ]);
+  describe('#createFiles', function() {
+    // it('should generate dot files');
   });
+
 });
